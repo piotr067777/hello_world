561f8a643cdd4bf0572c4015b3edd7d7597b5078
syrokomski piotr <syrokomskip@p20310.MINI>
2024-03-06 17:55:06 +0100

    initial version

---
+++ UnitTestProject1/StringCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public static class StringCalculator
+    {
+        public static int Calculate(string s)
+        {
+            if (s == "") return 0;
+
+            return Int32.Parse(s);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            Assert.AreEqual(StringCalculator.Calculate(""), 0);
+        }
+
+        [TestMethod]
+        public void SingleValueTest()
+        {
+            Assert.AreEqual(StringCalculator.Calculate("-1"), -1);
+            Assert.AreEqual(StringCalculator.Calculate("24"), 24);
+            Assert.AreEqual(StringCalculator.Calculate("0"), 0);
+            Assert.AreEqual(StringCalculator.Calculate("2137"), 2137);
+        }
+    }
+}
